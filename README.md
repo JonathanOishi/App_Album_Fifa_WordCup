e21a8c31ef99e3c447c218b4c3683034505b7c2f
Jonathan Oishi <jonathan_oishi@hotmail.com>
2022-10-17 13:23:55 -0300

    Primeiro Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Fifa World Cup Flutter Project
+
+Aplicativo de gerenciamento de albuns de figurinhas da copa do Qatar 2022.
+8a Edição do Dart Week com Rodrigo Rahman
---
+++ lib/app/pages/splash/view/splash_view_impl.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/widgets.dart';
+import 'package:fwc_album_app/app/core/ui/helpers/loader.dart';
+import 'package:fwc_album_app/app/pages/splash/splash_page.dart';
+
+import './splash_view.dart';
+
+abstract class SplashViewImpl extends State<SplashPage>
+    with Loader<SplashPage>
+    implements SplashView {
+  @override
+  void initState() {
+    widget.presenter.view = this;
+    super.initState();
+  }
+
+  @override
+  void logged(bool isLogged) {
+    hideLoader();
+    Navigator.of(context).pushNamedAndRemoveUntil(
+        isLogged ? '/home' : '/auth/login', (route) => false);
+  }
+}
